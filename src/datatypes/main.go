261394ac3859fe67647a4d82632e385e827424b2
Andrei Olar <andrei.olar@complyadvantage.com>
2020-05-26 10:25:16 +0300

    Change example so that implicit comparison doesn't compile anymore

--- src/datatypes/main.go
+++ src/datatypes/main.go
@@ -2,10 +2,11 @@ package main
 
 type Cmp struct {
 	field string
+	arr   []string
 }
 
 func main() {
-	a := Cmp{field: "a"}
-	b := Cmp{field: "a"}
+	a := Cmp{field: "a", arr: []string{"1", "2"}}
+	b := Cmp{field: "a", arr: []string{"1", "2"}}
 	println(a == b)
 }
